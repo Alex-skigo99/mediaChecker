3c098019ba80c024ff55b87639f77f3941660a17
Alexander BOLSHUKHIN <skigo99@gmail.com>
2025-07-27 21:22:11 +0300

    refactor: updateMediaTextAnalysis

--- utils/updateMediaTextAnalysis.js
+++ utils/updateMediaTextAnalysis.js
@@ -1,18 +1,15 @@
 import knex from "/opt/nodejs/db";
 import DatabaseTableConstants from "/opt/nodejs/DatabaseTableConstants";
 
-export async function updateMediaTextAnalysis(mediaId, hasTooMuchText) {
+export async function updateMediaTextAnalysis(id, hasTooMuchText) {
     try {
         await knex(DatabaseTableConstants.GMB_MEDIA_TABLE)
-            .where('id', mediaId)
+            .where("id", id)
             .update({
                 has_too_much_text: hasTooMuchText,
-                updated_at: knex.fn.now()
             });
-        
-        console.log(`Updated media ${mediaId} with has_too_much_text = ${hasTooMuchText}`);
+
     } catch (error) {
-        console.error(`Error updating media ${mediaId}:`, error);
         throw error;
     }
 }
